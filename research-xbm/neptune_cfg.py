d4a3509aad087e180bb8eaae00b25993ddc8785d
Polina Khanzhina <khanzhina.polina@icloud.come>
2022-10-30 19:30:15 +0300

    add cross-batch memory algo

---
+++ research-xbm/neptune_cfg.py
@@ -0,0 +1,7 @@
+from easydict import EasyDict
+
+neptune_cfg = EasyDict()
+neptune_cfg.project_name = 'pkhanzhina/finals'
+neptune_cfg.api_token = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIyMjI4ZDY3Mi0xZTdhLTRmODEtYTJhZS0yZGJiMWZmOWIxOGEifQ=="
+neptune_cfg.run_name = None
+
